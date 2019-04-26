bcfd4a417c347de1f9b228321aeb16abb283ccaf
Zamar Zahid <zamerzahid@gmail.com>
2019-04-27 00:38:49 +0500

    hms-add-package done

---
+++ src/main/java/com/example/test/DTO/PatientPackageDTO.java
@@ -0,0 +1,73 @@
+package com.example.test.DTO;
+
+import java.util.Date;
+
+public class PatientPackageDTO {
+    String pName;
+    String pFacility;
+    Date pStartDate;
+    Date pEndDate;
+    String pSponsor;
+    String status;
+
+    public PatientPackageDTO() {
+
+    }
+
+    public PatientPackageDTO(String pName, String pFacility, Date pStartDate, Date pEndDate, String pSponsor, String status) {
+        this.pName = pName;
+        this.pFacility = pFacility;
+        this.pStartDate = pStartDate;
+        this.pEndDate = pEndDate;
+        this.pSponsor = pSponsor;
+        this.status = status;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpFacility() {
+        return pFacility;
+    }
+
+    public void setpFacility(String pFacility) {
+        this.pFacility = pFacility;
+    }
+
+    public Date getpStartDate() {
+        return pStartDate;
+    }
+
+    public void setpStartDate(Date pStartDate) {
+        this.pStartDate = pStartDate;
+    }
+
+    public Date getpEndDate() {
+        return pEndDate;
+    }
+
+    public void setpEndDate(Date pEndDate) {
+        this.pEndDate = pEndDate;
+    }
+
+    public String getpSponsor() {
+        return pSponsor;
+    }
+
+    public void setpSponsor(String pSponsor) {
+        this.pSponsor = pSponsor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
